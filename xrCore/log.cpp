6ac4cb32ce2d1a26e2e282f99dba912638fb2753
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-08-28 13:45:17 +0300

    чёткий лог

--- xrCore/log.cpp
+++ xrCore/log.cpp
@@ -31,11 +31,11 @@ void FlushLog			()
 void AddOne				(const char *split) 
 {
 	logCS.Enter			();
-
-#ifdef DEBUG
-	OutputDebugString	(split);
-	OutputDebugString	("\n");
-#endif
+	if (IsDebuggerPresent())
+	{
+		OutputDebugString(split);
+		OutputDebugString("\n");
+	}
 
 	LogFile.push_back	(shared_str(split));
 
