9876cccce3ce8484f4e5b38f43f62163abf0bcc6
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-08-28 09:36:40 +0300

    линковка сосать

--- xr_3da/xrGame/script_space.h
+++ xr_3da/xrGame/script_space.h
@@ -17,11 +17,21 @@
 #pragma warning(disable:4530)
 #pragma warning(disable:4267)
 
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
+extern "C" {
+	#include <lua.h>
+	#include <lualib.h>
+	#include <lauxlib.h>
+	#include <luajit.h>
+};
+
 #include <luabind/luabind.hpp>
 #include <luabind/object.hpp>
+
+template <typename T1, typename T2>
+IC	T1 *_dynamic_cast(T2 *p2)
+{
+	return			(smart_cast<T1*>(p2));
+}
 
 extern	string4096	g_ca_stdout;
 
